c8f40d06d80da538c1569639cc1719803817d2d6
Nicoletti <nicoletti@cpqd.com.br>
2024-09-21 00:53:04 -0300

    add data de captura

--- routes/pokemons.js
+++ routes/pokemons.js
@@ -5,6 +5,12 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     Pokemon.find().then(pokemons => {
+
+        pokemons.forEach(pokemon => {
+            const data = new Date(pokemon._id.getTimestamp());
+            pokemon.capturadoEm = `${data.getUTCDate()}/${data.getUTCMonth() + 1}/${data.getUTCFullYear()}`;
+        })
+
         res.render('paginas/pokemons/index', {
             pokemons,
         });
