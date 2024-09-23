9f1e9e8633c996cf20b6160ce5b41f605ce83433
Nicoletti <nicoletti@cpqd.com.br>
2024-09-23 18:33:00 -0300

    crud update e find one

--- routes/api/pokemons.js
+++ routes/api/pokemons.js
@@ -26,7 +26,7 @@ router.get('/', async (req, res) => {
         const filtros = req.query;
         const options = {};
 
-        if(filtros.nomeComecaCom){
+        if (filtros.nomeComecaCom) {
             options.nome = {
                 $regex: filtros.nomeComecaCom + '.*'
             }
@@ -45,6 +45,44 @@ router.get('/', async (req, res) => {
     }
 })
 
+//Read (find one)
+router.get('/:id', async (req, res) => {
+    try {
+        const pokemon = await Pokemon.findOne({ _id: req.params.id });
+        res.status(201).json({
+            sucesso: true,
+            pokemon: pokemon
+        });
+    } catch (e) {
+        res.status(422).json({
+            sucesso: false,
+            erro: `pokémon não encontrado ${e}`
+        });
+    }
+})
 
+//Update
+router.patch('/:id', async (req, res) => {
+
+    try {
+        const pokemon = await Pokemon.findOne({ _id: req.params.id });
+
+        Object.keys(req.body).forEach(atributo => {
+            pokemon[atributo] = req.body[atributo];
+        })
+
+        await pokemon.save();
+
+        res.json({
+            sucess: true,
+            pokemon: pokemon,
+        })
+    } catch (e) {
+        res.status(422).json({
+            sucesso: false,
+            erro: e
+        });
+    }
+});
 
 module.exports = router;
