0e42014b2ed13a5fa9377e15c2cf6d0023524868
Nicoletti <nicoletti@cpqd.com.br>
2024-09-23 00:16:01 -0300

    configurando cors

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,4 +1,12 @@
 const express = require('express');
+const cors = require('cors');
+
+//configurando cors
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200,
+}
+
 
 // importando routes
 const capturaRouter = require('./captura');
@@ -7,7 +15,7 @@ const statusRouter = require('./status');
 const router = express.Router();
 
 // declarando as rotas
-router.use('/captura', capturaRouter);
-router.use('/status', statusRouter);
+router.use('/captura', cors(corsOptions), capturaRouter);
+router.use('/status', cors(corsOptions), statusRouter);
 
 module.exports = router;
